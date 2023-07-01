095e0b481ca531c4cb60d6f615651014c5c03d29
alb1nut <albertnutifafa@gmail.com>
2023-07-01 13:59:04 +0000

    completed Image gallery with Next Js Typescript and SUperbase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,51 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
+
+
+
+export async function getStaticProps() {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || '',
+    {
+      auth: {
+        persistSession: false,
+      },
+    }
+  )
+let { data } = await supabaseAdmin.from('images').select('*').order('id');
+ console.log(data)
+  return {
+    props: {
+      images : data
+    }
+  }
+}
+
+type Image = {
+  id:number
+  href:string
+  imageSrc: string
+  name: string
+  username:string
+}
 
 
 function cn(...classes:string[]){
   return classes.filter(Boolean).join(' ');
 }
 
-export default function Gallery() {
+export default function Gallery({images} : {images : Image[]}) {
   return (
     <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
 
       <div className='grid grid-cols-1 gap-y-10 sm:grid-col-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
-        <BlurImage />
+        {images.map(image =>(
+          <BlurImage key={image.id} image={image}/>
+        ))}
+        
       </div>
     </div>
   )
@@ -20,17 +53,17 @@ export default function Gallery() {
 
 
 
- function BlurImage() {
+ function BlurImage({image}: {image: Image}) {
   const [isLoading,setLoading] = useState(true);
   return (
-    <Link href='' className='group'>
+    <Link href={image.href} className='group'>
       <div className='w-full aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8  overflow-hidden rounded-lg bg-gray-200'>
         <Image 
         // width={500}
         // height={500}
         layout='fill'
         objectFit='cover'
-        src='https://bit.ly/placeholder-img'
+        src={image.imageSrc}
          alt='image' 
         className={cn(
           'group-hover:opacity-75 duration-700 ease-in-out', 
@@ -42,8 +75,8 @@ export default function Gallery() {
   onLoadingComplete={() =>setLoading(false)}
         />
       </div>
-      <h3 className='mt-4 text-sm text-gray-700'>Albert Dovlo</h3>
-      <p className='mt-1 text-lg font-medium text-gray-900'>@al_dov</p>
+      <h3 className='mt-4 text-sm text-gray-700'>{image.name}</h3>
+      <p className='mt-1 text-lg font-medium text-gray-900'>@{image.username}</p>
     </Link>
   )
 }
