28c9bfc4eb2c0bde9b1045939ffcbff31e643bff
alb1nut <albertnutifafa@gmail.com>
2023-07-01 12:42:45 +0000

    Image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Gallery() {
@@ -5,9 +7,27 @@ export default function Gallery() {
     <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
 
       <div className='grid grid-cols-1 gap-y-10 sm:grid-col-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
-        hello world
-        {/* {Images will go hee} */}
+        <ImageSHow />
       </div>
     </div>
+  )
+}
+
+
+
+ function ImageSHow() {
+  return (
+    <Link href='' className='group'>
+      <div className='aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200'>
+        <Image 
+        width={500}
+        height={500}
+        src='https://bit.ly/placeholder-img' alt='image' 
+        className='group-hover:opacity-75'
+        />
+      </div>
+      <h3 className='mt-4 text-sm text-gray-700'>Albert Dovlo</h3>
+      <p className='mt-1 text-lg font-medium text-gray-900'>@al_dov</p>
+    </Link>
   )
 }
